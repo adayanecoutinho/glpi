943074f7616d32f4785e71e27779a1b6d8419939
adayanecoutinho <adayanencl@gmail.com>
2015-03-25 16:59:09 -0300

    Subindo a pasta CONFIG

---
+++ glpi/config/config_db.php
@@ -0,0 +1,13 @@
+<?php
+ class DB extends DBmysql {
+                
+ var $dbhost = '';
+                
+ var $dbuser 	= 'root';
+                
+ var $dbpassword= 'root';
+                
+ var $dbdefault	= 'glpi_supri';
+                
+ } 
+?>
